7076cb055442065f9b3d166c30e50dadf7377818
Dan Stetsenko <oddestdan@gmail.com>
2021-05-24 00:56:27 +0300

    Project setup: config server-side

---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+const app = require("express")();
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ server/src/temp.spec.ts
@@ -0,0 +1,5 @@
+describe("Setting up the environment", () => {
+  it("should pass dummy Jest test", () => {
+    expect(1).toEqual(1);
+  });
+});
